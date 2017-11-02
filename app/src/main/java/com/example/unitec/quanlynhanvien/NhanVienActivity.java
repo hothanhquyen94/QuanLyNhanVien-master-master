26ab93051bb33d42b9c48edab4391d8629fa09f1
hothanhquyen94 <41203036@hcmut.edu.vn>
2017-11-02 17:43:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/unitec/quanlynhanvien/NhanVienActivity.java
@@ -0,0 +1,16 @@
+package com.example.unitec.quanlynhanvien;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Unitec on 02/11/2017.
+ */
+
+public class NhanVienActivity extends AppCompatActivity {
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_nhanvien);
+    }
+}
